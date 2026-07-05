7b1d8aeb38b1b26a3c7bda7ffe9f638c9957eaf3
BindusagarMG <122045445+BindusagarMG@users.noreply.github.com>
2026-07-05 14:27:29 +0530

    Update Motors.ino

--- Motor/Motors.ino
+++ Motor/Motors.ino
@@ -1,40 +1,43 @@
-#include <ESP32Servo.h>
-
-// For ESP32-1:
-const int SERVO_A_PIN = 4;
-const int SERVO_B_PIN = 5;
-
-// For ESP32-2:
-// const int SERVO_A_PIN = 1;
-// const int SERVO_B_PIN = 40;
-
-Servo servoA;
-Servo servoB;
+#define M1 48
+#define M2 47
+#define M3 21
+#define M4 14
+#define M5 13
+#define M6 12
+#define M7 11
+#define M8 10
 
 void setup() {
+  pinMode(M1, OUTPUT);
+  pinMode(M2, OUTPUT);
+  pinMode(M3, OUTPUT);
+  pinMode(M4, OUTPUT);
+  pinMode(M5, OUTPUT);
+  pinMode(M6, OUTPUT);
+  pinMode(M7, OUTPUT);
+  pinMode(M8, OUTPUT);
   Serial.begin(115200);
-
-  ESP32PWM::allocateTimer(0);
-  ESP32PWM::allocateTimer(1);
-
-  servoA.setPeriodHertz(50);
-  servoB.setPeriodHertz(50);
-
-  servoA.attach(SERVO_A_PIN, 500, 2400);
-  servoB.attach(SERVO_B_PIN, 500, 2400);
-
-  Serial.println("Servos Ready!");
+  Serial.println("Motor Driver Initialized");
 }
 
+void motorA_forward() { digitalWrite(M1, HIGH); digitalWrite(M2, LOW); }
+void motorA_backward() { digitalWrite(M1, LOW); digitalWrite(M2, HIGH); }
+void motorA_stop() { digitalWrite(M1, LOW); digitalWrite(M2, LOW); }
+
+void motorB_forward() { digitalWrite(M3, HIGH); digitalWrite(M4, LOW); }
+void motorB_backward() { digitalWrite(M3, LOW); digitalWrite(M4, HIGH); }
+void motorB_stop() { digitalWrite(M3, LOW); digitalWrite(M4, LOW); }
+
 void loop() {
-  for (int pos = 0; pos <= 180; pos += 1) {
-    servoA.write(pos);
-    servoB.write(pos);
-    delay(15);
-  }
-  for (int pos = 180; pos >= 0; pos -= 1) {
-    servoA.write(pos);
-    servoB.write(pos);
-    delay(15);
-  }
+  Serial.println("Moving Forward");
+  motorA_forward(); motorB_forward(); delay(3000);
+
+  Serial.println("Stopping");
+  motorA_stop(); motorB_stop(); delay(2000);
+
+  Serial.println("Reversing");
+  motorA_backward(); motorB_backward(); delay(3000);
+
+  Serial.println("Stopping");
+  motorA_stop(); motorB_stop(); delay(2000);
 }
